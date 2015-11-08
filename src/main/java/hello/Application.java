cfe55577bead49a51085d13a2b9d68a397a59739
Carlos Mantilla <ceo@aliongroo.com>
2015-11-08 17:54:31 +0200

    WIP: Tying to create endpoint for a service.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by carlosmantilla on 08/11/15.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
